e872048cc8cda7661c3c2d4ad9f76b93f05e605f
Matt Bessey <bessey@gmail.com>
2012-11-24 14:25:56 +0000

    Added token authentication

--- db/migrate/20121124141518_add_devise_tokens_to_user.rb
+++ db/migrate/20121124141518_add_devise_tokens_to_user.rb
@@ -1,4 +1,12 @@
 class AddDeviseTokensToUser < ActiveRecord::Migration
-  def change
+  
+  def self.up
+    change_table :users do |t|
+      t.string :authentication_token
+    end
   end
+  def self.down
+    t.remove :authentication_token
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121124134505) do
+ActiveRecord::Schema.define(:version => 20121124141518) do
 
   create_table "flats", :force => true do |t|
     t.string   "nickname"
@@ -22,5 +22,52 @@ ActiveRecord::Schema.define(:version => 20121124134505) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
+
+  create_table "messages", :force => true do |t|
+    t.integer  "from_id"
+    t.integer  "to_id"
+    t.integer  "flat_id"
+    t.text     "contents"
+    t.boolean  "received"
+    t.string   "context"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "shop_items", :force => true do |t|
+    t.integer  "flat_id"
+    t.integer  "user_want_id"
+    t.integer  "user_bought_id"
+    t.decimal  "price"
+    t.string   "name"
+    t.boolean  "paid_back"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "flat_id"
+    t.float    "geocode_lat"
+    t.float    "geocode_long"
+    t.string   "phone_number"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "authentication_token"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
