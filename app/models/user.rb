4e3af2967c5f79b35aac3f3e8765fd1b030cfb27
Matt Bessey <bessey@gmail.com>
2012-12-17 13:18:22 +0000

    fixed password error, and changed GCM api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :authentication_token
   attr_accessible :first_name, :flat_id, :geocode_lat, :geocode_long, :last_name, :phone_number
 
-  validates :email, :password, :first_name, :presence => true
+  validates :email, :first_name, :presence => true
   validates :email, :uniqueness => true
 
   belongs_to :flat
--- config/initializers/gcm_on_rails.rb
+++ config/initializers/gcm_on_rails.rb
@@ -1,4 +1,4 @@
 configatron.gcm_on_rails.api_url = 'https://android.googleapis.com/gcm/send'
-configatron.gcm_on_rails.api_key = 'AIzaSyBM0u_qmH0ap1ttgBMqF256OyWyUVfmPRQ'
+configatron.gcm_on_rails.api_key = 'AIzaSyB1jcNIwSoaU5YMflsNRnfl0Fotv2Wv-6Q'
 configatron.gcm_on_rails.app_name = 'com.Ballin_Octo_Hipster.flatmate'
 configatron.gcm_on_rails.delivery_format = 'json'
