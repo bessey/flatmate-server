8bcb3c19438c17504f0194c0b42fa49141700fd5
Matt Bessey <bessey@gmail.com>
2012-12-27 00:19:13 +0000

    Updated app name to be correct to new shortened version

--- config/initializers/gcm_on_rails.rb
+++ config/initializers/gcm_on_rails.rb
@@ -1,4 +1,4 @@
 configatron.gcm_on_rails.api_url = 'https://android.googleapis.com/gcm/send'
 configatron.gcm_on_rails.api_key = 'AIzaSyB1jcNIwSoaU5YMflsNRnfl0Fotv2Wv-6Q'
-configatron.gcm_on_rails.app_name = 'com.Ballin_Octo_Hipster.flatmate'
+configatron.gcm_on_rails.app_name = 'com.boh.flatmate'
 configatron.gcm_on_rails.delivery_format = 'json'
