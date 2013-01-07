ae494a79c8e8e16bf786823066d82f44ca44bea7
Matt Bessey <bessey@gmail.com>
2013-01-07 17:01:32 +0000

    Removed wrong changes to message

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,23 +5,22 @@ class Message < ActiveRecord::Base
   belongs_to :flat
 
   def send_out(sender)
-    recipient = User.joins(:gcm_device).where(:flat_id => sender.flat_id)
+    recipient_ids = 
+      User.joins(:gcm_device).where(:flat_id => sender.flat_id).map{ |u| u.gcm_device.registration_id }
     if sender.gcm_device
-      recipient.each do |recipient|
-        notification = recipient.gcm_device.notifications.build()
-        notification.data = {
-            #:registration_ids => recipient_ids,
-            :message => self.contents, 
-            :from_id => self.from_id,
-            :flat_id => self.flat_id,
-            :to_id => self.to_id,
-            :context => self.context,
-            :id => self.id
-            }
-        notification.collapse_key = "New messages"
-        notification.save
-        Gcm::Notification.send_notifications
-      end
+      notification = sender.gcm_device.notifications.build()
+      notification.data = {
+          :registration_ids => recipient_ids,
+          :message => self.contents, 
+          :from_id => self.from_id,
+          :flat_id => self.flat_id,
+          :to_id => self.to_id,
+          :context => self.context,
+          :id => self.id
+          }
+      notification.collapse_key = "New messages"
+      notification.save
+      Gcm::Notification.send_notifications
     end
   end
 
