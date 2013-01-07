7f1c9af7cc0e927e6eec5b737ce19f1d94f57c65
Matt Bessey <bessey@gmail.com>
2013-01-07 16:10:11 +0000

    Forgot an end

--- app/models/shop_item.rb
+++ app/models/shop_item.rb
@@ -9,19 +9,20 @@ class ShopItem < ActiveRecord::Base
   	recipient_ids = 
   		User.joins(:gcm_device).where(:flat_id => sender.flat_id).map{ |u| u.gcm_device.registration_id }
     if sender.gcm_device
-    notification = sender.gcm_device.notifications.build(
-      :data => {
-        :registration_ids => recipient_ids,
-        :item => self.name,
-        :user_want_id => self.user_want_id,
-        :user_bought_id => self.user_bought_id,
-        :price => self.price,
-        :paid_back => self.paid_back,
-        :id => self.id
-        },
-      :collapse_key => "New shopping")
-    notification.save
- 		Gcm::Notification.send_notifications
+      notification = sender.gcm_device.notifications.build(
+        :data => {
+          :registration_ids => recipient_ids,
+          :item => self.name,
+          :user_want_id => self.user_want_id,
+          :user_bought_id => self.user_bought_id,
+          :price => self.price,
+          :paid_back => self.paid_back,
+          :id => self.id
+          },
+        :collapse_key => "New shopping")
+      notification.save
+   		Gcm::Notification.send_notifications
+    end
   end
 
 end
