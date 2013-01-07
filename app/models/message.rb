b9b02fa6103d06a531384fc0d32c0177403c40d0
Matt Bessey <bessey@gmail.com>
2013-01-07 16:57:45 +0000

    Whoops

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,19 +8,20 @@ class Message < ActiveRecord::Base
     recipient = User.joins(:gcm_device).where(:flat_id => sender.flat_id)
     if sender.gcm_device
       recipient.each do |recipient|
-      notification = recipient.gcm_device.notifications.build()
-      notification.data = {
-          #:registration_ids => recipient_ids,
-          :message => self.contents, 
-          :from_id => self.from_id,
-          :flat_id => self.flat_id,
-          :to_id => self.to_id,
-          :context => self.context,
-          :id => self.id
-          }
-      notification.collapse_key = "New messages"
-      notification.save
-      Gcm::Notification.send_notifications
+        notification = recipient.gcm_device.notifications.build()
+        notification.data = {
+            #:registration_ids => recipient_ids,
+            :message => self.contents, 
+            :from_id => self.from_id,
+            :flat_id => self.flat_id,
+            :to_id => self.to_id,
+            :context => self.context,
+            :id => self.id
+            }
+        notification.collapse_key = "New messages"
+        notification.save
+        Gcm::Notification.send_notifications
+      end
     end
   end
 
