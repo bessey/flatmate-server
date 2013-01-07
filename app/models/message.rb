58b17762261813b7d191444bee5c017e4e368917
Matt Bessey <bessey@gmail.com>
2013-01-07 16:26:46 +0000

    Fixed message mass assignment

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,19 +7,21 @@ class Message < ActiveRecord::Base
   def send_out(sender)
     recipient_ids = 
       User.joins(:gcm_device).where(:flat_id => sender.flat_id).map{ |u| u.gcm_device.registration_id }
-    notification = sender.gcm_device.notifications.build(
-      :data => {
-        :registration_ids => recipient_ids,
-        :message => self.contents, 
-        :from_id => self.from_id,
-        :flat_id => self.flat_id,
-        :to_id => self.to_id,
-        :context => self.context,
-        :id => self.id
-        },
-      :collapse_key => "New messages")
-    notification.save
-    Gcm::Notification.send_notifications
+    if sender.gcm_device
+      notification = sender.gcm_device.notifications.build()
+      notification.data = {
+          :registration_ids => recipient_ids,
+          :message => self.contents, 
+          :from_id => self.from_id,
+          :flat_id => self.flat_id,
+          :to_id => self.to_id,
+          :context => self.context,
+          :id => self.id
+          }
+      notification.collapse_key = "New messages"
+      notification.save
+      Gcm::Notification.send_notifications
+    end
   end
 
   def self.test(sender)
