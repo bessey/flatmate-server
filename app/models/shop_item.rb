c01e8846c2364c98ec3bcc9f2821af21f4f8a6c5
Matt Bessey <bessey@gmail.com>
2013-01-07 17:11:26 +0000

    Shop item testing

--- app/models/shop_item.rb
+++ app/models/shop_item.rb
@@ -9,8 +9,8 @@ class ShopItem < ActiveRecord::Base
   	recipient_ids = 
   		User.joins(:gcm_device).where(:flat_id => sender.flat_id).map{ |u| u.gcm_device.registration_id }
     if sender.gcm_device
-      notification = sender.gcm_device.notifications.build(
-        :data => {
+      notification = sender.gcm_device.notifications.build()
+      notification.data = {
           :registration_ids => recipient_ids,
           :item => self.name,
           :user_want_id => self.user_want_id,
@@ -18,11 +18,21 @@ class ShopItem < ActiveRecord::Base
           :price => self.price,
           :paid_back => self.paid_back,
           :id => self.id
-          },
-        :collapse_key => "New shopping")
+          }
+      notification.collapse_key = "New shopping"
       notification.save
    		Gcm::Notification.send_notifications
     end
   end
+
+  def self.test(sender)
+    s = ShopItem.new(
+          :item => "Zaffa Cakes",
+          :user_want_id => sender.id,
+          )
+    s.save
+    s.send_out(sender)
+  end
+
 
 end
