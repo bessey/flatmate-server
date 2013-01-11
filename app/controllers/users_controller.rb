c0af44dfa467feba3b18c2f3fa8dca4fc9a881d8
Matt Bessey <bessey@gmail.com>
2013-01-11 16:40:52 +0000

    Remove user creation authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:create]
   before_filter :me, :only => [:show,:edit,:update,:destroy]
 
   def me
